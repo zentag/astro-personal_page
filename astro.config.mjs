dc6776f2ca325fd8df7465ec3fe543dda53b3e9b
zentag <38821230+zentag@users.noreply.github.com>
2025-04-20 07:53:44 -0400

    add fulldev ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import fulldevUi from 'fulldev-ui';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [fulldevUi()]
 });
