d28f1ebef6f3402e53ed1bf818d3ed103bf98190
zentag <38821230+zentag@users.noreply.github.com>
2025-04-21 08:09:44 -0400

    add qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import qwikdev from "@qwikdev/astro";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [qwikdev()],
 });
