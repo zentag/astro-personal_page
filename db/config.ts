71cbcd35a168aedc80cc553ad95ea19f3a27d181
zentag <38821230+zentag@users.noreply.github.com>
2025-04-29 10:07:18 -0400

    add dislikes and likes display

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,22 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
+const Posts = defineTable({
+  columns: {
+    file_name: column.text({ primaryKey: true }),
+    likes: column.number(),
+    dislikes: column.number(),
+  },
+});
+
+const Comments = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    postName: column.text({ references: () => Posts.columns.file_name }),
+    content: column.text(),
+    author: column.text(),
+  },
+});
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Posts, Comments },
 });
